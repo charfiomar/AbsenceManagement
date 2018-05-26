2001f363b3179a2d63aef054fe84ae7b5bcc8e19
BOSSf <faiez162@gmail.com>
2018-05-26 02:27:01 +0100

    controllers structure

---
+++ src/tn/api/omar/controllers/SpecialityController.java
@@ -0,0 +1,31 @@
+package tn.api.omar.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/speciality")
+public class SpecialityController {
+
+	@RequestMapping(value = { "/list" }, method = RequestMethod.GET)
+	public ModelAndView list() {
+		return null;
+	}
+
+	@RequestMapping(value = { "/add" }, method = RequestMethod.GET)
+	public ModelAndView add() {
+		return null;
+	}
+
+	@RequestMapping(value = { "/edit" }, method = RequestMethod.GET)
+	public ModelAndView edit() {
+		return null;
+	}
+
+	@RequestMapping(value = { "/delete" }, method = RequestMethod.GET)
+	public ModelAndView delete() {
+		return null;
+	}
+}
